21f00317f1334b915b4933aae3dd9b35295e55f8
fAemeister <sinangurcan26@hotmail.com>
2022-10-27 03:34:08 +0300

    .

--- src/pages/api/cart/index.ts
+++ src/pages/api/cart/index.ts
@@ -24,41 +24,39 @@ const Cart = async (req: NextApiRequest, res: NextApiResponse) => {
         where: { id: userId },
         select: { reservation: true },
       });
-      // let result = false;
-
-      // for (let i = 0; i < userDine?.reservation?.length; i++) {
-      //   const e = userDine?.reservation[i];
-      //   for (let x = 0; x < req.body.dine.length; x++) {
-      //     const _x = req.body.dine[x];
-      //     if (_x.id === e?.dineId) {
-      //       result = true;
-      //       break;
-      //     }
-      //   }
-      //   if (result) {
-      //     res.status(400).json({ error: "Already exists" });
-      //     return;
-      //   }
-
-        await prisma.cart.update({
-          where: { userId },
-          data: {
-            dine: {
-              disconnect: req.body.dine.map((e) => ({ id: e.id })),
-            },
-          },
-        });
-        await prisma.reservedDine.createMany({
-          data: req.body.dine.map((e) => {
-            return {
-              userId,
-              dineId: e.id,
-            };
-          }),
-        });
-        res.status(200).send("OK");
-        break;
+      let result = false;
+      for (let i = 0; i < userDine?.reservation?.length; i++) {
+        const e = userDine?.reservation[i];
+        for (let x = 0; x < req.body.dine.length; x++) {
+          const _x = req.body.dine[x];
+          if (_x.id === e?.dineId) {
+            result = true;
+            break;
+          }
+        }
       }
+      if (result) {
+        res.status(400).json({ error: "Already exists" });
+        return;
+      }
+      await prisma.cart.update({
+        where: { userId },
+        data: {
+          dine: {
+            disconnect: req.body.dine.map((e) => ({ id: e.id })),
+          },
+        },
+      });
+      await prisma.reservedDine.createMany({
+        data: req.body.dine.map((e) => {
+          return {
+            userId,
+            dineId: e.id,
+          };
+        }),
+      });
+      res.status(200).send("OK");
+      break;
   }
 };
 
