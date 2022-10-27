bd7d21d8a64c7fb9a2acae4f5a9a84ec1a3a7e18
fAemeister <sinangurcan26@hotmail.com>
2022-10-27 03:21:32 +0300

    adding reservation to cart bug fixed

--- src/pages/api/cart/index.ts
+++ src/pages/api/cart/index.ts
@@ -20,24 +20,45 @@ const Cart = async (req: NextApiRequest, res: NextApiResponse) => {
       res.status(200).json(cart);
       break;
     case "POST":
-      await prisma.cart.update({
-        where: { userId },
-        data: {
-          dine: {
-            disconnect: req.body.dine.map((e) => ({ id: e.id })),
+      const userDine = await prisma.user.findFirst({
+        where: { id: userId },
+        select: { reservation: true },
+      });
+      let result = false;
+
+      for (let i = 0; i < userDine?.reservation?.length; i++) {
+        const e = userDine?.reservation[i];
+        for (let x = 0; x < req.body.dine.length; x++) {
+          const _x = req.body.dine[x];
+          if (_x.id === e?.dineId) {
+            result = true;
+            break;
+          }
+        }
+        if (result) {
+          res.status(400).json({ error: "Already exists" });
+          return;
+        }
+
+        await prisma.cart.update({
+          where: { userId },
+          data: {
+            dine: {
+              disconnect: req.body.dine.map((e) => ({ id: e.id })),
+            },
           },
-        },
-      });
-      await prisma.reservedDine.createMany({
-        data: req.body.dine.map((e) => {
-          return {
-            userId,
-            dineId: e.id,
-          };
-        }),
-      });
-      res.status(200).send("OK");
-      break;
+        });
+        await prisma.reservedDine.createMany({
+          data: req.body.dine.map((e) => {
+            return {
+              userId,
+              dineId: e.id,
+            };
+          }),
+        });
+        res.status(200).send("OK");
+        break;
+      }
   }
 };
 
