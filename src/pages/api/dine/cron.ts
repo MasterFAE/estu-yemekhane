a93112eff4e5b2a61a301b7807e6b9a4d91f7b84
fAemeister <sinangurcan26@hotmail.com>
2022-11-06 21:39:18 +0300

    cronjob added

--- src/pages/api/dine/cron.ts
+++ src/pages/api/dine/cron.ts
@@ -4,71 +4,64 @@ import addDays from "../../../lib/addDays";
 import addDaysToDay from "../../../lib/addDaysToDay";
 import { prisma } from "../../../server/db/client";
 
-const cronJob = new CronJob(
-  "0 0 * * SAT",
-  async () => {
-    const foods = await prisma?.food.findMany();
-    const soupList = foods?.filter((e) => e.type === FoodType.SOUP);
-    const mainList = foods?.filter((e) => e.type === FoodType.MAIN);
-    const sideList = foods?.filter((e) => e.type === FoodType.SIDE);
-    const extraList = foods?.filter((e) => e.type === FoodType.EXTRA);
-    var d = new Date();
-    let monday = d.setDate(d.getDate() + ((1 + 7 - d.getDay()) % 7));
-    let newDineList = [];
-    for (let i = 0; i < 5; i++) {
-      newDineList.push({
-        date: new Date(addDaysToDay(monday, i)),
-        type: DINEHOURS.BREAKFAST,
-        foods: {
-          connect: [
-            { id: getRandomFood(soupList) },
-            { id: getRandomFood(mainList) },
-            { id: getRandomFood(sideList) },
-            { id: getRandomFood(extraList) },
-          ],
-        },
-      });
-      newDineList.push({
-        date: new Date(addDaysToDay(monday, i)),
-        type: DINEHOURS.LUNCH,
-        foods: {
-          connect: [
-            { id: getRandomFood(soupList) },
-            { id: getRandomFood(mainList) },
-            { id: getRandomFood(sideList) },
-            { id: getRandomFood(extraList) },
-          ],
-        },
-      });
-      newDineList.push({
-        date: new Date(addDaysToDay(monday, i)),
-        type: DINEHOURS.DINNER,
-        foods: {
-          connect: [
-            { id: getRandomFood(soupList) },
-            { id: getRandomFood(mainList) },
-            { id: getRandomFood(sideList) },
-            { id: getRandomFood(extraList) },
-          ],
-        },
-      });
-    }
-    newDineList.forEach(async (e) => {
-      await prisma?.dine.create({
-        data: {
-          type: e.type,
-          date: e.date,
-          foods: { connect: e.foods.connect.map((x) => ({ id: x.id })) },
-        },
-      });
-    });
-  },
-  null,
-  true
-);
-
 const dineCronHandler = async (req, res) => {
-  res.send(cronJob.nextDates().toString());
+  const foods = await prisma?.food.findMany();
+  const soupList = foods?.filter((e) => e.type === FoodType.SOUP);
+  const mainList = foods?.filter((e) => e.type === FoodType.MAIN);
+  const sideList = foods?.filter((e) => e.type === FoodType.SIDE);
+  const extraList = foods?.filter((e) => e.type === FoodType.EXTRA);
+  var d = new Date();
+  let monday = d.setDate(d.getDate() + ((1 + 7 - d.getDay()) % 7));
+  let newDineList = [];
+  for (let i = 0; i < 5; i++) {
+    newDineList.push({
+      date: new Date(addDaysToDay(monday, i)),
+      type: DINEHOURS.BREAKFAST,
+      foods: {
+        connect: [
+          { id: getRandomFood(soupList) },
+          { id: getRandomFood(mainList) },
+          { id: getRandomFood(sideList) },
+          { id: getRandomFood(extraList) },
+        ],
+      },
+    });
+    newDineList.push({
+      date: new Date(addDaysToDay(monday, i)),
+      type: DINEHOURS.LUNCH,
+      foods: {
+        connect: [
+          { id: getRandomFood(soupList) },
+          { id: getRandomFood(mainList) },
+          { id: getRandomFood(sideList) },
+          { id: getRandomFood(extraList) },
+        ],
+      },
+    });
+    newDineList.push({
+      date: new Date(addDaysToDay(monday, i)),
+      type: DINEHOURS.DINNER,
+      foods: {
+        connect: [
+          { id: getRandomFood(soupList) },
+          { id: getRandomFood(mainList) },
+          { id: getRandomFood(sideList) },
+          { id: getRandomFood(extraList) },
+        ],
+      },
+    });
+  }
+  await newDineList.forEach(async (e) => {
+    await prisma?.dine.create({
+      data: {
+        type: e.type,
+        date: e.date,
+        foods: { connect: e.foods.connect.map((x) => ({ id: x.id })) },
+      },
+    });
+  });
+
+  res.send("Completed");
 };
 
 function getRandomFood(array) {
